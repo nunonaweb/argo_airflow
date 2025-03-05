518cca94cc9634e06e4fa16f1b1bc96daf3262fa
Bruno <contato@brunoborges.eti.br>
2025-03-05 15:56:45 -0300

    sdfsdfsdf

--- encoding.py
+++
@@ -1,6 +0,0 @@
-import base64
-encoded_user = base64.b64encode(b'nunonaweb')
-encoded_pass = base64.b64encode(b'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')
-print(encoded_user)
-print(encoded_pass)
-
